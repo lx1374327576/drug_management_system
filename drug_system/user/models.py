f9222e596856df07c82ca3055af1767c5f818f68
lx1374327576 <41529712+lx1374327576@users.noreply.github.com>
2019-04-19 16:20:33 +0800

    add user-admin and change the model

--- drug_system/user/models.py
+++ drug_system/user/models.py
@@ -3,9 +3,13 @@ from django.utils import timezone
 
 
 class User(models.Model):
+
     id = models.IntegerField('id', primary_key=True)
     user_name = models.CharField('user_name', unique=True, blank=False, max_length=20)
     create_time = models.DateTimeField('create_time', default=timezone.now)
-    is_admin = models.BinaryField('is_admin', default=False)
+    is_admin = models.BooleanField('is_admin', default=False)
     password = models.CharField('password', blank=False, max_length=20)
     worker_type = models.IntegerField('worker_type', default=0)
+
+    def __str__(self):
+        return self.user_name
