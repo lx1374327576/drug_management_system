fb71ee1f3ee233d2de168b912241e553e22d431d
lx1374327576 <41529712+lx1374327576@users.noreply.github.com>
2019-04-19 15:09:46 +0800

    add model User

--- drug_system/user/models.py
+++ drug_system/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.IntegerField('id', primary_key=True)
+    user_name = models.CharField('user_name', unique=True, blank=False, max_length=20)
+    create_time = models.DateTimeField('create_time', default=timezone.now)
+    is_admin = models.BinaryField('is_admin', default=False)
+    password = models.CharField('password', blank=False, max_length=20)
+    worker_type = models.IntegerField('worker_type', default=0)
