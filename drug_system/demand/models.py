3646623512f918ff17707a63ba787b57e40c6b17
lx1374327576 <41529712+lx1374327576@users.noreply.github.com>
2019-05-09 11:46:23 +0800

    add demand

--- drug_system/demand/models.py
+++ drug_system/demand/models.py
@@ -15,17 +15,14 @@ class Medicine(models.Model):
 class Detail(models.Model):
 
     id = models.IntegerField('id', primary_key=True)
-    father_id = models.IntegerField('father_id')
-    father_detail_id = models.IntegerField('father_detail_id')
+    father_id = models.IntegerField('father_id', default=0)
+    father_detail_id = models.IntegerField('father_detail_id', default=0)
     medicine = models.ForeignKey(Medicine, blank=False, on_delete=models.CASCADE)
     num = models.FloatField('num')
     price = models.FloatField('price')
     status = models.IntegerField('status', default=0)  # 0 未处理 1 处理中 2 流程结束
     form_type = models.IntegerField('form_type')  # 1 需求单 2 出库单 3 缺货单 4 采购单 5 入库单
     form_id = models.IntegerField('form_id')
-
-    def __str__(self):
-        return self.id
 
 
 class Demander(models.Model):
@@ -40,15 +37,12 @@ class Demander(models.Model):
 class Demand(models.Model):
 
     id = models.IntegerField('id', primary_key=True)
-    due_time = models.DateTimeField('due_time')
+    due_time = models.DateTimeField('due_time', default=timezone.now)
     demander = models.ForeignKey(Demander, on_delete=models.CASCADE)
     create_time = models.DateTimeField('create_time', default=timezone.now)
     create_user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.IntegerField('status', default=0)  # 0 未处理 1 审核通过 2 单子完成 -1 审核未通过
-    check_time = models.DateTimeField('check_time')
-
-    def __str__(self):
-        return self.id
+    check_time = models.DateTimeField('check_time', default=timezone.now)
 
 
 class Demander_Demand(models.Model):
--- drug_system/demand/views.py
+++ drug_system/demand/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Demander_Demand
+from .models import Demander_Demand, Detail, Demand, Demander
+from user.models import User
 
 
 # Create your views here.
@@ -8,9 +9,35 @@ def demand(request):
     context = {
         "demand_list": demands,
     }
-    print('context', context)
+    # print('context', context)
     return render(request, 'demand/index.html', context)
 
 
 def submit(request):
-    pass
+    demand_map = request.POST
+    lx = User.objects.get(id=1)
+    demander = Demander.objects.get(id=1)
+    demand_form = Demand(create_user=lx, demander=demander)
+    demand_form.save()
+    demand_form_list = Demand.objects.all()
+    length = demand_form_list.count()  # 获取表数据总长度
+    demand_form = demand_form_list[length-1]
+    detail_list = []
+    flag = False
+    for key in demand_map:
+        if not flag:
+            flag = True
+            continue
+        single = Demander_Demand.objects.get(id=key)
+        detail = Detail(price=single.id*single.num, num=single.num, medicine=single.medicine,
+                        form_type=1, form_id=demand_form.id)
+        detail.save()
+        de_list = Detail.objects.all()
+        length = de_list.count()  # 获取表数据总长度
+        detail = de_list[length - 1]
+        detail_list.append(detail)
+    context = {
+        "demand_form": demand_form,
+        "detail_list": detail_list,
+    }
+    return render(request, 'demand/submit.html', context)
