c7ce4c6972a9f683f8b4a790d7cbda6340b51c06
lx1374327576 <41529712+lx1374327576@users.noreply.github.com>
2019-05-10 13:43:01 +0800

    change 需求 to 出库计划

--- drug_system/purchase/urls.py
+++ drug_system/purchase/urls.py
@@ -6,4 +6,8 @@ app_name = 'purchase'
 
 urlpatterns = [
     path('purchase/', views.purchase, name='purchase'),
+    path('submit/', views.submit, name='submit'),
+    path('make/', views.make, name='make'),
+    path('query/', views.query, name='query'),
+    path('query_detail/<int:purchase_id>/', views.query_detail, name='query_detail'),
 ]
--- drug_system/purchase/views.py
+++ drug_system/purchase/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 def purchase(request):
     context = {}
     return render(request, 'purchase/index.html', context)
+
+
+def submit(request):
+    pass
+
+
+def make(request):
+    pass
+
+
+def query(request):
+    pass
+
+
+def query_detail(request, purchase_id):
+    pass
